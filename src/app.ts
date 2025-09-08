37d7dcb360084c7d9fe83a7b1eb7e4210b6b807d
botesz <majoros.botesz@gmail.com>
2025-09-08 09:24:36 +0200

    vs2

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+const createNumber = (value: number): number => {
+    return Math.floor(Math.random()* value+1);
+};
+
+const setNumberList = (value: number, v:number, createNumber: (v:number) => number): number[] => {
+    const t: number[] = [];
+    for (let i = 0; i < value; i++) {
+        t.push(createNumber(v))
+    }
+    return t;
+}
+
+const setNumberList2 = (count: number, v:number, createNumber: (v:number) => number): number[] => {
+    return Array.from({length: count}, item => createNumber(v) );
+}
+
+const numbers = setNumberList(100, 5, createNumber);
+console.log(numbers);
+
+
+type Stat = Record<number, number>;
+const statistic: Stat = {};
+//statistic["1000"] = 59;
+
+for (let v of numbers) {
+    if (v in statistic) {
+        statistic[v]! += 1;
+    } else {
+        statistic[v] = 1;
+    }
+}
